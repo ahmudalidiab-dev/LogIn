521b1489ea6fd144069f2c03d13fcbe6e5b16f5d
Ali43 <ahmudalidiab@gmail.com>
2025-10-21 09:52:31 +0300

    init

---
+++ app/src/main/java/com/example/login/FirebaseServices.java
@@ -0,0 +1,37 @@
+package com.example.login;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class FirebaseServices {
+    private static FirebaseServices instance;
+    private FirebaseAuth auth;
+    private FirebaseFirestore fire;
+    private FirebaseStorage storage;
+
+    public FirebaseServices(){
+        auth = FirebaseAuth.getInstance();
+        fire = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+    public FirebaseFirestore getFire() {
+        return fire;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public static FirebaseServices getInstance(){
+        if (instance==null){
+            instance= new FirebaseServices();
+        }
+        return instance;
+    }
+}
